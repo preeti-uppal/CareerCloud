9dbdedc106289ede4388fe394d63b201a02e4127
Preeti Uppal <preetikatyal24@gmail.com>
2018-05-27 21:13:26 -0500

    Adding missed files

---
+++ CareerCloud.Pocos/SecurityRolePoco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.Pocos
+{[Table("Security_Roles")]
+    class SecurityRolePoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Role { get; set; }
+        [Column("Is_Inactive")]
+        public bool IsInactive { get; set; }
+
+    }
+}
